2d0856c2f55a9982b8ba1db5e30b4032f8e09839
Matthew Poulter <Matthew.Poulter@rsg.local>
2016-04-12 13:06:15 +0100

    Input boxes changed to text areas and JS changed to split each box into arrays

--- Question Generator/questionGenerator.js
+++ Question Generator/questionGenerator.js
@@ -22,13 +22,43 @@ function gbt(ta) {
 var tempArray = new Array();
 
 gbi('create').addEventListener('click', function(){
-	var qu = gbi('question').value;
-	var a1 = gbi('answerOne').value;
-	var a2 = gbi('answerTwo').value;
-	var a3 = gbi('answerThree').value;
-	var a4 = gbi('answerFour').value;
-	var c1 = gbi('correct').value;
-	tempArray[tempArray.length] = new tempQuestion (qu, a1, a2, a3, a4, c1);
-	var stringedTempArr = JSON.stringify(tempArray);
-	gbi('results').innerHTML = stringedTempArr;
+    var questionSplit = gbi('question').value.split('\n');
+    var firstAnswerSplit = gbi('answerOne').value.split('\n');
+    var secondAnswerSplit = gbi('answerTwo').value.split('\n');
+    var thirdAnswerSplit = gbi('answerThree').value.split('\n');
+    var fourthAnswerSplit = gbi('answerFour').value.split('\n');
+    var correctAnswerSplit = gbi('correct').value.split('\n');
+    
+    for (var i = 0; i < questionSplit.length; i++){
+        var qu = questionSplit[i];
+        var a1 = firstAnswerSplit[i];
+        var a2 = secondAnswerSplit[i];
+        var a3 = thirdAnswerSplit[i];
+        var a4 = fourthAnswerSplit[i];
+        var c1 = correctAnswerSplit[i];
+        tempArray[tempArray.length] = new tempQuestion (qu, a1, a2, a3, a4, c1);
+    }
+     var stringedTempArr = JSON.stringify(tempArray);
+        gbi('results').innerHTML = stringedTempArr;
 })
+
+
+
+
+
+
+
+// OLD get questions
+//gbi('create').addEventListener('click', function(){
+//	var qu = gbi('question').value;
+//	var a1 = gbi('answerOne').value;
+//	var a2 = gbi('answerTwo').value;
+//	var a3 = gbi('answerThree').value;
+//	var a4 = gbi('answerFour').value;
+//	var c1 = gbi('correct').value;
+//	tempArray[tempArray.length] = new tempQuestion (qu, a1, a2, a3, a4, c1);
+//	var stringedTempArr = JSON.stringify(tempArray);
+//	gbi('results').innerHTML = stringedTempArr;
+//    
+//})
+
